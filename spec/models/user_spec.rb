be4c99584c9928743d80175fa0f5a5a988d07848
otukutun <orehaorz@gmail.com>
2014-05-12 23:47:56 +0900

    spec追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new(username: "Example User",email: "user@example.com", password:"foobar", password_confirmation: "foobar")
+  end
+
+  subject { @user }
+  it { should respond_to(:username) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 end
